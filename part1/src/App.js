4504e767e24c8769e3ddaf90865d2a1697f0fa36
Dave Boland <dboland77@gmail.com>
2021-08-16 09:04:02 +0100

    Rendering a component multiple times

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,13 +1,19 @@
-
-
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b= 20;
+const Hello = () => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p> {a} plus {b} is {a+b}</p>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </div>
   );
 };
