f4112d8693a4a74abb11be364fc45256bc24a3b1
Dave Boland <dboland77@gmail.com>
2021-08-16 09:02:45 +0100

    Adding variables to output

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,15 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+
+
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b= 20;
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p> {a} plus {b} is {a+b}</p>
+    </div>
+  );
+};
 
 export default App;
