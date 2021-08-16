4a9e19e31ca1d6bc79ba2ab5ab1eac872027869e
Dave Boland <dboland77@gmail.com>
2021-08-16 09:07:44 +0100

    Adding multiple props:

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,19 @@
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
     </div>
   )
 }
 
 const App = () => {
-
+  const name = "Peter"
+  const age = 10
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name = "George"/>
-      <Hello name = "James"/>
-      <Hello name = "Daisy"/>
+      <Hello name = "Maya" age ={26+10}/>
+      <Hello name = {name} age = {age}/>
     </div>
   );
 };
