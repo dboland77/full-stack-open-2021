fe5e4bebf650122e7b13e8412d91e7bc5ccc491b
Dave Boland <dboland77@gmail.com>
2021-08-16 10:00:22 +0100

    Refactor to use parts

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,6 +4,16 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
     <p>
       {props.part} {props.exercises}
     </p>
@@ -31,9 +41,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} exercises1={exercises1} 
+       part2={part2} exercises2={exercises2} 
+       part3={part3} exercises3={exercises3} />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
