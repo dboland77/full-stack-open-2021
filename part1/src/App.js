0f5078d003a70cade325ee3d6eba1086a579e4f3
Dave Boland <dboland77@gmail.com>
2021-08-16 09:05:24 +0100

    Component with props

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,7 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}</p>
     </div>
   )
 }
@@ -11,9 +11,9 @@ const App = () => {
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello/>
-      <Hello/>
-      <Hello/>
+      <Hello name = "George"/>
+      <Hello name = "James"/>
+      <Hello name = "Daisy"/>
     </div>
   );
 };
